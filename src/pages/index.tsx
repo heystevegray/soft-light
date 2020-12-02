ef975867b59f27fe21b6cdafdf1504fda43c5c5d
Steve <heystevegray@gmail.com>
2020-12-02 02:23:35 -0500

    Added description and messages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,14 @@ interface State {
   usePalette: boolean
 }
 
+const messages = [
+  "Free lighting what's up",
+  "Adjust the background color of this page to match your lighting conditions",
+  "soft light, for everyone",
+  "soft light, it's lit",
+  "Look how beautiful you are!",
+]
+
 const STORAGE_KEY = "soft-light-data"
 
 const initialState: State = {
@@ -38,25 +46,32 @@ export default function Home() {
     (storage && JSON.parse(storage)) || initialState
   )
   const [usePalette, setUsePalette] = useState(state.usePalette || false)
+  const [showPicker, setShowPicker] = useState(false)
+  const [messageIndex, setMessageIndex] = useState(0)
 
   useEffect(() => {
     saveLocalStorage()
   }, [state])
 
+  const getRandomNumber = (min = 0, max = messages.length - 1): number => {
+    const randomNumber = Math.random() * (max - min + 1) + min
+    return Math.floor(randomNumber)
+  }
+
   const handleColorChange = (color: ColorResult): void => {
     setState(state => ({ ...state, backgroundColor: color }))
   }
 
-  const togglePalette = (): void => {
-    const value = !usePalette
-    setUsePalette(value)
-    setState(state => ({ ...state, usePalette: value }))
+  const togglePalette = (usePalette: boolean): void => {
+    setMessageIndex(getRandomNumber())
+    setUsePalette(usePalette)
+    setState(state => ({ ...state, usePalette: usePalette }))
+    setShowPicker(true)
   }
 
   const saveDefault = (): void => {
-    console.log("Saving...")
-
     setState({ ...state, defaultColor: state.backgroundColor })
+    setShowPicker(false)
   }
 
   const saveLocalStorage = (): void => {
@@ -83,7 +98,9 @@ export default function Home() {
   const useStyles = makeStyles(theme => ({
     text: {
       padding: theme.spacing(2, 2, 0),
-      fontSize: "1.5em",
+      fontSize: "1.25em",
+      color: "var(--light)",
+      textShadow: "var(--textShadow)",
     },
     appBar: {
       backgroundColor: "var(--dark)",
@@ -115,30 +132,53 @@ export default function Home() {
     <div className="page" style={{ background: formatColor(hex, alpha) }}>
       <div className="container">
         <div className="header">
-          <Typography className={classes.text} variant="h1" gutterBottom>
+          <Typography
+            tabIndex={1}
+            aria-label="soft light"
+            className={classes.text}
+            variant="h1"
+            gutterBottom
+          >
             soft light
           </Typography>
         </div>
-        <div className="body">
-          <div className="pickers">
-            {!usePalette && (
-              <ChromePicker
-                className="picker"
-                color={state.backgroundColor.rgb}
-                onChange={handleColorChange}
-                onChangeComplete={handleColorChange}
-              />
-            )}
-            {usePalette && (
-              <SwatchesPicker
-                styles={swatchStyles}
-                color={state.backgroundColor.rgb}
-                onChange={handleColorChange}
-                onChangeComplete={handleColorChange}
-              />
-            )}
-          </div>
-        </div>
+        {showPicker && (
+          <>
+            <div className="description">
+              <div className="description__container">
+                <Typography
+                  tabIndex={2}
+                  aria-label="description of soft light"
+                  className={classes.text}
+                  variant="body1"
+                  gutterBottom
+                >
+                  {messages[messageIndex]}
+                </Typography>
+              </div>
+            </div>
+            <div className="body">
+              <div className="pickers">
+                {!usePalette && (
+                  <ChromePicker
+                    className="picker"
+                    color={state.backgroundColor.rgb}
+                    onChange={handleColorChange}
+                    onChangeComplete={handleColorChange}
+                  />
+                )}
+                {usePalette && (
+                  <SwatchesPicker
+                    styles={swatchStyles}
+                    color={state.backgroundColor.rgb}
+                    onChange={handleColorChange}
+                    onChangeComplete={handleColorChange}
+                  />
+                )}
+              </div>
+            </div>
+          </>
+        )}
         <div className="footer">
           <AppBar
             position="relative"
@@ -151,7 +191,7 @@ export default function Home() {
                 edge="start"
                 color="inherit"
                 aria-label="Colorize"
-                onClick={togglePalette}
+                onClick={() => togglePalette(false)}
               >
                 <ColorizeIcon />
               </IconButton>
@@ -172,7 +212,7 @@ export default function Home() {
                 edge="end"
                 aria-label="Palette"
                 color="inherit"
-                onClick={togglePalette}
+                onClick={() => togglePalette(true)}
               >
                 <PaletteIcon />
               </IconButton>
